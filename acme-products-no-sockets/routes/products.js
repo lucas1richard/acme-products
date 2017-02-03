c46d45911e2501bfedb55c893bcbb496e879fd09
Eric Katz <ericpkatz@gmail.com>
2017-02-02 19:21:26 -0500

    Update products.js

--- acme-products-no-sockets/routes/products.js
+++ acme-products-no-sockets/routes/products.js
@@ -10,10 +10,12 @@ router.get('/', (req, res) => {
   res.render('products', { products: productsModel.list() });
 });
 
+//if you are going to use bodyParser you are probably going to use it for the entire app
+//you shouldn't have to manually add the middlewear here..
 // Form post to the /products page
 router.post('/', urlencodedParser, (req, res) => {
-  let name = req.body.name;
-  let product = productsModel.addProduct(name);
+  let name = req.body.name;//why let - does it change?
+  let product = productsModel.addProduct(name);//this seems odd to me... why calling twice
   productsModel.addProduct(product);
   res.redirect( '/products' );
 });
@@ -21,8 +23,8 @@ router.post('/', urlencodedParser, (req, res) => {
 // Navigate to a specific product and show the edit form
 router.get('/:id/edit', (req, res) => {
   let id = req.params.id;
-  let product = productsModel.getProduct(id);
-  res.render('edit', { product });
+  let product = productsModel.getProduct(id);//const
+  res.render('edit', { product });//good
 });
 
 // Form post to the specific product page (delete)
@@ -33,6 +35,7 @@ router.delete('/:id', (req, res) => {
 });
 
 // Form post to the specific product page (edit)
+//again configure body parser for the app
 router.put('/:id', urlencodedParser, (req, res) => {
   let id = req.params.id * 1;
   req.body.id = id;
